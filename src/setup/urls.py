70de1a6c17cd3fa51b5336fbb778b593e71e3da8
Havallon Azevedo <havallon.ecomp@gmail.com>
2024-04-07 21:25:12 -0300

    add jwt auth

--- src/setup/urls.py
+++ src/setup/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('person.urls')),
+    path('api/', include('person.auth_urls')),
 ]
